cb4c6a3cfd85d5a70d6f651fd70b40885f0aa8ce
Shuvo-Roy <shuvoroysp@gmail.com>
2022-06-11 18:57:37 +0600

    adding local storage

--- main.js
+++ main.js
@@ -29,11 +29,12 @@ let shopItemsData=[{
     img:"images/tshirt4.jpg",
 },];
 
-let basket =[];
+let basket = JSON.parse(localStorage.getItem("data"))  || [];
 
 let generateShop=()=>{
     return (shop.innerHTML= shopItemsData.map((x)=>{
         let {id,name,price,desc,img}=x;
+        let search = basket.find((x)=> x.id === id) || []
         return `<div class="item">
         <img width="220" height="250" src=${img} alt="">
         <div id=product-id-${id} class="details">
@@ -43,7 +44,7 @@ let generateShop=()=>{
                 <h2>$ ${price}</h2>
                 <div class="button">
                     <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-                    <div id=${id} class="quantitu">0</div>
+                    <div id=${id} class="quantitu">${search.item== undefined? 0: search.item}</div>
                     <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
                 </div>
             </div>
@@ -70,7 +71,7 @@ let increment = (id)=>{
         search.item+=1;
     }
 
-    
+    localStorage.setItem("data", JSON.stringify(basket));
     //console.log(basket);
     update(selectedItem.id);
 };
@@ -86,7 +87,7 @@ let decrement = (id)=>{
         search.item-=1;
     }
 
-    
+    localStorage.setItem("data", JSON.stringify(basket));
     //console.log(basket);
     update(selectedItem.id);
 };
@@ -108,3 +109,5 @@ let calculation=()=>{
     let cartIcon=document.getElementById("cartAmount");
     cartIcon.innerHTML= basket.map((x) =>x.item).reduce((x,y) =>x+y,0);
 };
+
+calculation();
