d38ee7842581c9b750638d8f6e9507694fa555a4
Shuvo-Roy <shuvoroysp@gmail.com>
2022-06-11 19:35:03 +0600

    cart store

---
+++ src/data.js
@@ -0,0 +1,28 @@
+let shopItemsData=[{
+    id:"item1",
+    name:"Blue T-shirt",
+    price:45,
+    desc:"Lorem, ipsum dolor sit amet consectetur adipisicing.",
+    img:"images/tshirt1.jpg",
+},
+{
+    id:"item2",
+    name:"Black T-shirt",
+    price:40,
+    desc:"Lorem, ipsum dolor sit amet consectetur adipisicing.",
+    img:"images/tshirt2.jpg",
+},
+{
+    id:"item3",
+    name:"Casual Shirt",
+    price:50,
+    desc:"Lorem, ipsum dolor sit amet consectetur adipisicing.",
+    img:"images/tshirt3.jpg",
+},
+{
+    id:"item4",
+    name:"Design T-shirt",
+    price:55,
+    desc:"Lorem, ipsum dolor sit amet consectetur adipisicing.",
+    img:"images/tshirt4.jpg",
+},];
--- src/main.js
+++ src/main.js
@@ -1,33 +1,6 @@
 let shop = document.getElementById("shop");
 
-let shopItemsData=[{
-    id:"item1",
-    name:"Blue T-shirt",
-    price:45,
-    desc:"Lorem, ipsum dolor sit amet consectetur adipisicing.",
-    img:"images/tshirt1.jpg",
-},
-{
-    id:"item2",
-    name:"Black T-shirt",
-    price:40,
-    desc:"Lorem, ipsum dolor sit amet consectetur adipisicing.",
-    img:"images/tshirt2.jpg",
-},
-{
-    id:"item3",
-    name:"Casual Shirt",
-    price:50,
-    desc:"Lorem, ipsum dolor sit amet consectetur adipisicing.",
-    img:"images/tshirt3.jpg",
-},
-{
-    id:"item4",
-    name:"Design T-shirt",
-    price:55,
-    desc:"Lorem, ipsum dolor sit amet consectetur adipisicing.",
-    img:"images/tshirt4.jpg",
-},];
+
 
 let basket = JSON.parse(localStorage.getItem("data"))  || [];
 
