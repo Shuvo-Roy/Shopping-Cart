2f09f6b10539480cfec9abaaace1df38b71bf055
Shuvo-Roy <shuvoroysp@gmail.com>
2022-06-12 19:26:02 +0600

    complete

--- src/cart.js
+++ src/cart.js
@@ -16,14 +16,15 @@ let generateCartItems =()=>{
         return shoppingCart.innerHTML = basket.map((x)=>{
             let {id,item } =x;
             let search =shopItemsData.find((y)=>y.id=== id) || [];
+            let {img,name,price}=search;
             return `
             <div class="cart-item">
-        <img width="100" src=${search.img} alt="" />
+        <img width="100" src=${img} alt="" />
         <div class="details">
           <div class="title-price-x">
               <h4 class="title-price">
-                <p>${search.name}</p>
-                <p class="cart-item-price">$ ${search.price}</p>
+                <p>${name}</p>
+                <p class="cart-item-price">$ ${price}</p>
               </h4>
               <i onclick="removeItem(${id})" class="bi bi-trash"></i>
           </div>
@@ -105,6 +106,7 @@ let removeItem=(id)=>{
     basket = basket.filter((x)=>x.id !==selectedItem.id);
     generateCartItems();
     toltalAmount();
+    calculation();
     localStorage.setItem("data", JSON.stringify(basket));
 };
 
@@ -129,5 +131,6 @@ toltalAmount();
 let clearCart=()=>{
     basket=[];
     generateCartItems();
+    calculation();
     localStorage.setItem("data", JSON.stringify(basket));
 };
