42c11fabc200dada72f3f1cc241e618fad768f10
Shuvo-Roy <shuvoroysp@gmail.com>
2022-06-12 18:34:11 +0600

    removing items

--- src/cart.js
+++ src/cart.js
@@ -12,5 +12,96 @@ calculation();
 
 
 let generateCartItems =()=>{
+    if(basket.length !==0){
+        return shoppingCart.innerHTML = basket.map((x)=>{
+            let {id,item } =x;
+            let search =shopItemsData.find((y)=>y.id=== id) || [];
+            return `
+            <div class="cart-item">
+        <img width="100" src=${search.img} alt="" />
+        <div class="details">
+          <div class="title-price-x">
+              <h4 class="title-price">
+                <p>${search.name}</p>
+                <p class="cart-item-price">$ ${search.price}</p>
+              </h4>
+              <i onclick="removeItem(${id})" class="bi bi-trash"></i>
+          </div>
+          <div class="button">
+              <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+              <div id=${id} class="quantity">${item}</div>
+              <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+          </div>
+          <h3>$ ${item * search.price}</h3>
+        </div>
+      </div>
+      `;
+      })
+      .join("");
+    }
+    else{
+        shoppingCart.innerHTML=``
+        label.innerHTML=`
+        <h2>Cart is Empty</h2>
+        <a href="index.html">
+        <button class="HomeBtn">Back to Home</button>
+
+        </a>
+        `;
+    }
+};
+generateCartItems();
+
+let increment = (id)=>{
+    let selectedItem=id;
+    let search=basket.find((x)=>x.id===selectedItem.id);
+
+    if(search===undefined){
+        basket.push({
+        id:selectedItem.id,
+        item:1,
+    });
+    }
+    else{
+        search.item+=1;
+    }
+
     
-}
+    generateCartItems();
+    update(selectedItem.id);
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+
+//decrement function
+
+let decrement = (id)=>{
+    let selectedItem=id;
+    let search=basket.find((x)=>x.id===selectedItem.id);
+
+    if(search === undefined) return;
+    else if(search.item===0) return;
+    else{
+        search.item-=1;
+    }
+
+    update(selectedItem.id);
+    basket= basket.filter((x)=>x.item!==0);
+    generateCartItems();
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let update = (id)=>{
+    let search = basket.find((x)=>x.id === id);
+    console.log(search.item);
+
+    document.getElementById(id).innerHTML=search.item;
+
+    calculation();
+};
+
+let removeItem=(id)=>{
+    let selectedItem = id;
+    basket = basket.filter((x)=>x.id !==selectedItem.id);
+    generateCartItems();
+    localStorage.setItem("data", JSON.stringify(basket));
+};
